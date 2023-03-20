a6a1f655bbbd005847ab060864ed2027c621f931
bahamgit <amadou.bah2@epsi.fr>
2023-03-20 16:38:02 +0100

    test

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
-clefAnsible="ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKTU4LbNRT7d441d9SA5yCzyUnSzveXs+aXUFgVcSN/TJ880HKoINpJGml1IoAIRbRIM5na8OUgNflPungvYHlY= user-ansible@ansible"
-sudo mkdir /home/ansible
-sudo mkdir /home/ansible/.ssh
-sudo useradd -d /home/ansible -s /usr/bin/bash ansible
-sudo chown ansible:ansible /home/ansible
-sudo chown ansible:ansible /home/ansible/.ssh
-sudo chmod 750 /home/ansible
-echo "ansible ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/ansible
-echo ${clefAnsible} ANSIBLE-AGENT | sudo tee -a /home/ansible/.ssh/authorized_keys
-sudo chown ansible:ansible /home/ansible/.ssh/authorized_keys
-sudo chmod 600 /home/ansible/.ssh/authorized_keys
+clefuser-ansible="ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKTU4LbNRT7d441d9SA5yCzyUnSzveXs+aXUFgVcSN/TJ880HKoINpJGml1IoAIRbRIM5na8OUgNflPungvYHlY= user-user-ansible@user-ansible"
+sudo mkdir /home/user-ansible
+sudo mkdir /home/user-ansible/.ssh
+sudo useradd -d /home/user-ansible -s /usr/bin/bash user-ansible
+sudo chown user-ansible:user-ansible /home/user-ansible
+sudo chown user-ansible:user-ansible /home/user-ansible/.ssh
+sudo chmod 750 /home/user-ansible
+echo "user-ansible ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/user-ansible
+echo ${clefuser-ansible} user-ansible-AGENT | sudo tee -a /home/user-ansible/.ssh/authorized_keys
+sudo chown user-ansible:user-ansible /home/user-ansible/.ssh/authorized_keys
+sudo chmod 600 /home/user-ansible/.ssh/authorized_keys
 sudo apt update
 sudo apt install python3 -y
 sudo reboot
