6a69299efec3c7a85037f6337548eeac788a039f
bahamgit <amadou.bah2@epsi.fr>
2023-03-21 12:12:14 +0100

    test

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-clefuser_ansible="ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKTU4LbNRT7d441d9SA5yCzyUnSzveXs+aXUFgVcSN/TJ880HKoINpJGml1IoAIRbRIM5na8OUgNflPungvYHlY= user-user-ansible@user-ansible"
+clefuser_ansible="ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKTU4LbNRT7d441d9SA5yCzyUnSzveXs+aXUFgVcSN/TJ880HKoINpJGml1IoAIRbRIM5na8OUgNflPungvYHlY= user-ansible@ansible"
 sudo mkdir /home/user-ansible
 sudo mkdir /home/user-ansible/.ssh
 sudo useradd -d /home/user-ansible -s /usr/bin/bash user-ansible
@@ -7,7 +7,7 @@ sudo chown user-ansible:user-ansible /home/user-ansible
 sudo chown user-ansible:user-ansible /home/user-ansible/.ssh
 sudo chmod 750 /home/user-ansible
 echo "user-ansible ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/user-ansible
-echo ${clefuser-ansible} user-ansible-AGENT | sudo tee -a /home/user-ansible/.ssh/authorized_keys
+echo ${clefuser_ansible} | sudo tee -a /home/user-ansible/.ssh/authorized_keys
 sudo chown user-ansible:user-ansible /home/user-ansible/.ssh/authorized_keys
 sudo chmod 600 /home/user-ansible/.ssh/authorized_keys
 sudo apt update
