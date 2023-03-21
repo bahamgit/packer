8ed28a693c87191b7c11b52ff09466946a88e72d
bahamgit <amadou.bah2@epsi.fr>
2023-03-21 15:42:55 +0100

    test

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ sudo chown user-ansible:user-ansible /home/user-ansible
 sudo chown user-ansible:user-ansible /home/user-ansible/.ssh
 sudo chmod 750 /home/user-ansible
 echo "user-ansible ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/user-ansible
-echo ${clefuser_ansible} | sudo tee -a /home/user-ansible/.ssh/authorized_keys
+echo "${clefuser_ansible}" | sudo tee -a /home/user-ansible/.ssh/authorized_keys
 sudo chown user-ansible:user-ansible /home/user-ansible/.ssh/authorized_keys
 sudo chmod 600 /home/user-ansible/.ssh/authorized_keys
 sudo apt update
